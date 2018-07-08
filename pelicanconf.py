0f4a398e51fb2bb54d03bc7aacd7283682d260ad
Artium Nihamkin <artium@nihamkin.com>
2018-07-09 00:01:10 +0300

    Temporary removing projects

--- pelicanconf.py
+++ pelicanconf.py
@@ -26,7 +26,7 @@ AUTHOR_FEED_RSS = None
 
 # Blogroll
 LINKS = (('Blog', '/'),
-         ('Projects', '/projects'),
+         #('Projects', '/projects'),
          ('About', '/2012/09/01/about-this-blog/#about-this-blog') )
 
 # Social widget
